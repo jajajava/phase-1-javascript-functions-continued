2c7add3ff5e025eb432eaeb0b622709e873b20d2
David <davidstepanov@yahoo.com>
2022-08-30 15:20:43 -0400

    commit tree 7663787ab6aa86352c462cbb80b5363edec5b8f6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    console.log(`This Saturday, I want to ${activity}`)
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(activity= "go to the office"){
+    console.log(`This Monday, I will ${activity}`)
+}
+mondayWork("work from home")
+
+
+function wrapAdjective(s="*"){
+    return function(phrase= "special"){
+      const programmer = "programmer"
+      return `You are ${s}a ${phrase} ${programmer}${s}! `
+    }
+  }  
+
+console.log(wrapAdjective("||")("dedicated"))
